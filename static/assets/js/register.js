bc659dfb948aa8b1634da548aa742125018f8ef8
Marksuuuu <112992662+Marksuuuu@users.noreply.github.com>
2023-06-14 16:20:55 +0800

    swal added
    swal added

--- static/assets/js/register.js
+++ static/assets/js/register.js
@@ -22,19 +22,45 @@ $(document).ready(function(){
 		formData.append('password', password);
 		formData.append('role',role);
 
-		
-		console.log(role)
-
 		$.ajax({
 			url: '/register',
 			method: 'POST',
 			data: formData,
 			contentType: false,
 			processData: false,
-
-			success: function(data){
+			beforeSend: function(){
+				$('#waitMeDiv').waitMe({
+					effect: 'rotateplane',
+					text: 'Please wait...',
+					bg: 'rgba(255,255,255,0.7)',
+					color: '#435ebe',
+					maxSize: '',
+					waitTime: -1,
+					textPos: 'vertical',
+					fontSize: '',
+					source: ''
+				});
 
 			},
+			success: function(data){
+				Swal.fire({
+					icon: 'success',
+					title: 'Registered!',
+					showConfirmButton: true,
+					text: 'Registered Successfully!.',
+				})
+				var personUsername = $('#personUsername').val('')
+				var firstname = $('#personfirstName').val('')
+				var lastname = $('#personlastName').val('')
+				var email = $('#personEmail').val('')
+				var password = $('#personPassword').val('')
+				var repeat_password = $('#personRepeatPassword').val('')
+				var role = $('#role').val('')
+			},
+
+		}).done(function (){
+			$('#waitMeDiv').waitMe('hide');
+
 
 		});
 	}
